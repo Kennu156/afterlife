9bce87092074a820dfd0402f3bb20d15acd1f60a
Kennu156 <kenmarttipaju@gmail.com>
2025-03-21 05:02:13 +0200

    magazine

--- src/views/Magazine.vue
+++ src/views/Magazine.vue
@@ -6,4 +6,15 @@ import Navigation from '../components/navigation.vue'
 
 <template>
     <Navigation/>
+    <section class="mt-10">
+        <h1 class="text-9xl text-center items-center size"><strong>MAGAZINE</strong></h1>
+    </section>
 </template>
+
+<style scoped>
+
+
+.size {
+    font-size: 230px;
+}
+</style>
